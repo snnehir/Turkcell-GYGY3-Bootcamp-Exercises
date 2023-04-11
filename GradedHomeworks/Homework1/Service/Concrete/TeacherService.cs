a8f64ab3e6a5d1a7907f221acb56671bf4a988f8
snnehir <snnehir21@gmail.com>
2023-04-12 00:49:52 +0300

    Initial commit

---
+++ GradedHomeworks/Homework1/Service/Concrete/TeacherService.cs
@@ -0,0 +1,48 @@
+﻿public class TeacherService : ITeacherService
+{
+    List<Teacher> teachers;
+    public TeacherService(List<Teacher> teachers)
+    {
+        this.teachers = teachers;
+    }
+    public TeacherService()
+    {
+        teachers = new();
+    }
+    public void Add(Teacher entity)
+    {
+        teachers.Add(entity);
+    }
+    public Teacher? GetById(int Id)
+    {
+        return teachers.FirstOrDefault(x => x.Id == Id);
+    }
+
+    public List<Teacher> GetAll() => teachers;
+
+    public void Remove(int Id)
+    {
+        Teacher? teacher = GetById(Id);
+        if (teacher == null)
+            throw new Exception("Teacher not found.");
+        else
+        {
+            teachers.Remove(teacher);
+        }
+    }
+
+    public void Update(Teacher entity)
+    {
+        Teacher? teacher = GetById(entity.Id);
+        if (teacher == null)
+            throw new Exception("Student not found.");
+        else
+        {
+            int index = teachers.IndexOf(teacher);
+            teachers[index] = entity;
+        }
+    }
+
+    public List<Teacher> GetTeachersByFullName(string firstName, string lastName) => teachers.Where(t => t.FirstName == firstName && t.LastName == lastName).ToList();
+
+}
---
+++ README.md
@@ -0,0 +1,4 @@
+# Turkcell-GYGY3-Bootcamp-Exercises
+This repository contains exercises and enhanced versions of graded homework assignments provided during the Turkcell GYGY 2023 .NET Bootcamp.
+
+Also visit my forked repository 👉🏻 [turkcellGY3](https://github.com/snnehir/turkcellGY3)
